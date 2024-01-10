828dbb3ce5a7b88158604ce2256f63c6e5c7cb10
Areeb-L4-python <areeb.zulfiqar19@gmail.com>
2024-01-10 11:48:47 +0400

    CH4

---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,8 @@
+favorite_fruits=["Bananas","Apple","Mangoes"]
+if 'Bananas' in favorite_fruits:
+    print("you really like Bananas")
+if 'Apples' in favorite_fruits:
+    print("you really like Apples")
+if 'Mangoes' in favorite_fruits:
+    print("you really like Mangoes")
+    
